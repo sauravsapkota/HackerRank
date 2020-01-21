3b1ffa720ad0e724a1d6423e8037e62196d86b21
sauravsapkota <saurav0030@gmail.com>
2020-01-21 16:30:46 +0545

    Added Apple and Orange.py File

---
+++ Practice/Tutorials/30 Days of Code/Day 1: Data Types.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# There is no variable declaration in Python! You just assign a value to a variable and it comes into existence. But never use a variable name without having assigned a value beforehand.
+
+# Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+
+# Print the sum of the double variables on a new line.
+print(d + d2)
+
+# Concatenate and print the String variables on a new line
+print(s + s2)
+# The 's' variable above should be printed first.
+
