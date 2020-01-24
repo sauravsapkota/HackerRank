2596aac3b92b05820b1a961bb41fee37fd5fac60
sauravsapkota <saurav0030@gmail.com>
2020-01-24 16:53:17 +0545

    added "  Utopian Tree.py" File

---
+++ Practice/Algorithms/Implementation/Utopian Tree.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import os
+
+
+# Complete the utopianTree function below.
+def utopianTree(n):
+    height = 1
+    for i in range(0, n):
+        if i % 2 == 0:
+            height *= 2
+        else:
+            height += 1
+
+    return height
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
