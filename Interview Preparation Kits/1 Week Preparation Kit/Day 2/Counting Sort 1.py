50faf28278c4e2fee0f10e2f6a8343ff01f99f0a
Saurav Sapkota <saurav.sapkota@infodevelopers.com.np>
2022-04-26 17:12:54 +0545

    Added 'Counting Sort 1.py' File

---
+++ Interview Preparation Kits/1 Week Preparation Kit/Day 2/Counting Sort 1.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    sol=[0]*100  # creates an array filled with 0s
+    for a in arr :
+        sol[a] += 1
+
+    return sol
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
